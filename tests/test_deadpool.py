0064f8d6f4d23ad2ad5fc470b1980898d2651aee
Cameron <561860+wallies@users.noreply.github.com>
2023-04-19 22:03:50 +1000

    Update test_deadpool.py

--- tests/test_deadpool.py
+++ tests/test_deadpool.py
@@ -7,22 +7,66 @@ import unittest
 import signal
 import time
 
-from concurrent.futures import CancelledError, as_completed
+from concurrent.futures import CancelledError, as_completed, ThreadPoolExecutor
 from contextlib import contextmanager
 from unittest.mock import Mock
 
 import deadpool
 
 
-class TestThreadPool(unittest.TestCase):
-    def test_run_in_executor(self):
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+async def test_func():
+    await asyncio.sleep(0.1)
+    return 42
 
-        with deadpool.Deadpool() as pool:
-            mock_callback = Mock()
-            loop.run_in_executor(pool, mock_callback)
-            mock_callback.assert_called_once()
+
+class TestDeadPool(unittest.TestCase):
+    async def test_acquire(self):
+        async with deadpool.DeadPool(size=2) as pool:
+            fut1 = pool.acquire()
+            fut2 = pool.acquire()
+            await asyncio.sleep(0.1)  # ensure both workers are running
+            self.assertEqual(pool.num_workers(), 2)
+            pool.release(await fut1)
+            pool.release(await fut2)
+        self.assertEqual(pool.num_workers(), 0)
+
+    async def test_concurrent_tasks(self):
+        async with deadpool.DeadPool(size=2) as pool:
+            results = await asyncio.gather(pool.run_in_executor(test_func),
+                                           pool.run_in_executor(test_func))
+            self.assertEqual(len(results), 2)
+            self.assertIn(42, results)
+
+    async def test_oversubscription(self):
+        async with deadpool.DeadPool(size=2) as pool:
+            fut1 = pool.acquire()
+            fut2 = pool.acquire()
+            fut3 = pool.acquire()
+            await asyncio.sleep(0.1)  # ensure all workers are running
+            self.assertEqual(pool.num_workers(), 2)
+            pool.release(await fut1)
+            pool.release(await fut2)
+            pool.release(await fut3)
+        self.assertEqual(pool.num_workers(), 0)
+
+    async def test_closed_pool(self):
+        pool = deadpool.DeadPool(size=2)
+        await pool.acquire()
+        await pool.acquire()
+        pool.close()
+        with self.assertRaises(RuntimeError):
+            await pool.acquire()
+
+    async def test_worker_exceptions(self):
+        async def failing_func():
+            await asyncio.sleep(0.1)
+            raise ValueError("failed")
+        async with deadpool.DeadPool(size=2) as pool:
+            results = await asyncio.gather(pool.run_in_executor(failing_func),
+                                           pool.run_in_executor(test_func))
+            self.assertEqual(len(results), 2)
+            self.assertIn(42, results)
+            self.assertIsInstance(results[0], ValueError)
 
 
 def f():
